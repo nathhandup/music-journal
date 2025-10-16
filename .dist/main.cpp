2976e3ff7ef017ed62511e5bc805fb90e3167544
nathhandup <natedup8@gmail.com>
2025-10-15 21:11:25 -0400

    changed compiler to g++, added basic file reading, created data.txt

--- .dist/main.cpp
+++ .dist/main.cpp
@@ -2,7 +2,21 @@
 #include <string.h>
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(void) {
+    std::fstream MyFile("data.txt");
+    if(MyFile.is_open()) {
+        //write to file
+        MyFile << "Beginning.\n";
+        MyFile.seekg(0);
+        std::string line;
+        while(std::getline(MyFile, line)) {
+            std::cout << line << std::endl;
+        }
+        MyFile.close();
+    } else {
+        std::cerr << "Failed to open file. Check existence.\n";
+    }
     return 0;
 }
